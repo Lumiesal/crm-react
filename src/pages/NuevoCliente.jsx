fef0670946db26d6f7b26d362f9af7fd5103a2c3
unknown <lumiesal@gmail.com>
2023-12-06 17:22:30 -0500

    commit principal

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -24,6 +24,7 @@ const NuevoCliente = () => {
             type="submit"
             className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
             value="Registras CLiente"
+            //
           />
         </form>
       </div>
